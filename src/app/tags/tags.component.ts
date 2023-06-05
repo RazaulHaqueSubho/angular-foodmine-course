c44d8373f82f4025a8813776f5ee711faad321dd
RazaulHaqueSubho <md.razaul.haque.subho@rwth-aachen.de>
2023-06-05 15:30:37 +0200

    feat:add searchBar and Tags for filter

---
+++ src/app/tags/tags.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Tag } from '../shared/models/tag';
+import { FoodService } from '../services/food/food.service';
+
+@Component({
+  selector: 'app-tags',
+  templateUrl: './tags.component.html',
+  styleUrls: ['./tags.component.css']
+})
+export class TagsComponent {
+  tags: Tag[] = []
+
+  constructor(private foodservice: FoodService) { }
+
+  ngOnInit() {
+    this.tags = this.foodservice.getAllTag();
+  }
+}
